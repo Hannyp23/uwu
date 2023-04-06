b14b72aaff490382b8c653ef87049ffc300a4365
Hannyp23 <130109686+Hannyp23@users.noreply.github.com>
2023-04-06 18:16:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector("#yesBtn");
+
+yesBtn.addEventListener("click",function () {
+    alert("Imposible que digas que si :c")
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('Sabia que ibas a decir que no, tonto :c')
+});
